4828a37f100beb12ba91ae2d8e48e47e3e942af4
schemr <schemryj@gmail.com>
2017-12-29 18:01:59 +0900

    Added side drawer component and responsive style

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../Ui/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+
+const sideDrawer = (props) => {
+    return (
+        <Aux>
+            <Backdrop show />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    )
+};
+
+export default sideDrawer;
