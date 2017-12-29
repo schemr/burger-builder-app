2e285b32c3a895d97baeffae84a23779f711dcea
schemr <schemryj@gmail.com>
2017-12-29 17:09:19 +0900

    Added logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Logo.css';
+import logoImage from '../../assets/images/burger-logo.png'
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={logoImage} alt="logo" />
+    </div>
+);
+
+export default logo;
