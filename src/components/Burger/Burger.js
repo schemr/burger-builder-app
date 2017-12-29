0455198d9a56056c39aa39898c60398d5a8a5709
schemr <schemryj@gmail.com>
2017-12-29 14:01:52 +0900

    Create build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css'
 
 const burger = (props) => {
+    let transIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+            return <BurgerIngredient key={ingredientKey+i} type={ingredientKey} />
+        })
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if (transIngredients.length === 0){
+        transIngredients = <p>Please Start Burger Builder</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
