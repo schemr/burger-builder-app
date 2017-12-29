0b157fb578152045666cf438d96e610da916bc9d
schemr <schemryj@gmail.com>
2017-12-29 18:24:02 +0900

    Added drawer toggle menu and style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer
+            }
+        })
+    }
 
-export default layout;
+    render() {
+        return (
+            <Aux>
+                <Toolbar 
+                    drawerToggleclicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+    
+}
+
+export default Layout;
