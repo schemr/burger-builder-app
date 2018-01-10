7d0fff63a9a6e98eab938cc86a8ca861c197a31e
schemr <schemryj@gmail.com>
2018-01-10 16:14:19 +0900

    Auth page added loading and error message

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,7 @@ import classes from './Auth.css';
 
 import Input from '../../components/Ui/Input/Input';
 import Button from '../../components/Ui/Button/Button';
+import Spinner from '../../components/Ui/Spinner/Spinner';
 
 import * as actions from '../../store/actions/index';
 
@@ -97,7 +98,7 @@ class Auth extends Component {
                 config: this.state.controls[key]
             });
         }
-        const form = formElementArray.map(formElement => (
+        let form = formElementArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType} 
@@ -109,8 +110,21 @@ class Auth extends Component {
                 touched={formElement.config.touched}
                 changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ));
+
+        if(this.props.loading) {
+            form = <Spinner />
+        }
+
+        let errorMessage = null;
+        if(this.props.error) {
+            errorMessage = (
+                <p className={classes.Error}>{this.props.error.message}</p>
+            )
+        }
+
         return(
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -123,10 +137,16 @@ class Auth extends Component {
     }
 };
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
